768a85fa911e21dce52b588168a33c3ec674cfac
Yunus Gedik <yunus-gedik@outlook.com>
2025-01-17 00:04:22 +0300

    feat: add required views

---
+++ User Viewer/Model/Geo.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct Geo: Codable {
+    let lat: Double
+    let lng: Double
+}
---
+++ User Viewer/View/UserListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  User Viewer
+//
+//  Created by Yunus Gedik on 16.01.2025.
+//
+
+import UIKit
+
+class UserListViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
